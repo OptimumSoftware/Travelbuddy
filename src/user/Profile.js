ed5b250977802054342e5f8f9022b1c7db317039
Jelle de Vries <jelle_de_vriesie@hotmail.com>
2018-07-02 14:11:27 +0200

    Changes

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -220,8 +220,8 @@ class Favorites extends Component {
 			.then(() => {
 				this.state.items.forEach(favorite => {
 					if(favorite.placeId) {
-						axios.get(this.proxyUrl + this.placeDetailsUrl + favorite.placeId + this.key)
-								.then(response => {
+						axios.get(this.proxyUrl + this.placeDetailsUrl + favorite.placeId + this.key, {mode: 'no-cors'})
+							.then(response => {
 									var temp1 = this.state.placeDetails;
 									temp1[favorite.placeId] = response.data;
 									this.setState({
